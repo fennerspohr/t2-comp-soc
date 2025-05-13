0269da5d87783e1139266ec4cc09d57bb7d1423b
Nathália Zófoli <nathalia.oalmeida@gmail.com>
2025-05-13 16:21:11 -0300

    configurar debug e secret key em settings + criação .env

--- backend/bdedica/settings.py
+++ backend/bdedica/settings.py
@@ -11,6 +11,16 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from corsheaders.defaults import default_headers
+import os 
+import environ
+env = environ.Env()
+
+#reading .env-file
+environ.Env.read_env()
+
+DEBUG = env('DEBUG')
+SECRET_KEY = env('SECRET_KEY')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
