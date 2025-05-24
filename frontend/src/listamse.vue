2787cdeaedee8ed4abbb33be5c5fe64a6b725373
Luiza Manoelle <luizamanoelle@hotmail.com>
2025-05-24 00:22:49 -0300

    mudança nos botão, adição de botão de editar

--- frontend/src/listamse.vue
+++ frontend/src/listamse.vue
@@ -22,11 +22,12 @@
           <input type="search" required placeholder="Pesquisar" />
         </label>
         <!--botão de adicionar-->
-        <a href="#/about/cadastromse"className="btn btn-circle btn-sm">
-          <svg xmlns="http://www.w3.org/2000/svg" className="w-8 h-8" fill="none" viewBox="0 0 24 24" strokeWidth="2.5" stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+        <a href="#/about/cadastromse" class="btn btn-soft btn-primary">
+          <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 448 512" fill="currentColor">
+            <path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 144L48 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l144 0 0 144c0 17.7 14.3 32 32 32s32-14.3 32-32l0-144 144 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-144 0 0-144z"/>
           </svg>
         </a>
+
       </div>
     </div>
     <div class="overflow-x-auto">
@@ -56,24 +57,28 @@
             
             <!--modal de visualização-->
             <td>
-              <button class="btn btn-sm btn-circle" @click="abrirModal(id)">
-                <svg xmlns="http://www.w3.org/2000/svg" 
-                    class="size-[1.2em]" 
-                    fill="none" 
-                    viewBox="0 0 24 24" 
-                    stroke-width="2.5" 
-                    stroke="currentColor">
-                  <path stroke-linecap="round" 
-                        stroke-linejoin="round" 
-                        d="M2.25 12s3.75-6.75 9.75-6.75 
-                          9.75 6.75 9.75 6.75-3.75 6.75-9.75 
-                          6.75S2.25 12 2.25 12z" />
-                  <path stroke-linecap="round" 
-                        stroke-linejoin="round" 
-                        d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                </svg>
-              </button>
+          <button class="btn btn-soft btn-primary" @click="abrirModal(id)">
+            <svg xmlns="http://www.w3.org/2000/svg" 
+                class="w-5 h-5" 
+                viewBox="0 0 576 512" 
+                fill="currentColor">
+              <path d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64c0 35.3-28.7 64-64 64c-7.1 0-13.9-1.2-20.3-3.3c-5.5-1.8-11.9 1.6-11.7 7.4c.3 6.9 1.3 13.8 3.2 20.7c13.7 51.2 66.4 81.6 117.6 67.9s81.6-66.4 67.9-117.6c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3z"/>
+            </svg>
+          </button>
             </td>
+
+           <!--modal de editar-->
+            <td>
+          <a href="#/about/editarmse" class="btn btn-soft btn-primary">
+            <svg xmlns="http://www.w3.org/2000/svg" 
+                class="w-5 h-5" 
+                viewBox="0 0 512 512" 
+                fill="currentColor">
+              <path d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160L0 416c0 53 43 96 96 96l256 0c53 0 96-43 96-96l0-96c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 96c0 17.7-14.3 32-32 32L96 448c-17.7 0-32-14.3-32-32l0-256c0-17.7 14.3-32 32-32l96 0c17.7 0 32-14.3 32-32s-14.3-32-32-32L96 64z"/>
+            </svg>
+          </a>
+            </td>
+
           </tr>
         </tbody>
       </table>
