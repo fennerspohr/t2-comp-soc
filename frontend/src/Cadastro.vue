57a28d4fc1b6cb470cd11250db96d6eee9a0b0df
Gustavo <gustavo.pottdeoliveira@gmail.com>
2025-05-27 00:00:39 -0300

    tela de selecao de cadastro com botoes coloridos

--- frontend/src/Cadastro.vue
+++ frontend/src/Cadastro.vue
@@ -1,26 +1,25 @@
 <template>
-    <div class="min-h-screen flex flex-col md:flex-row items-center justify-center gap-10 pg-6 bg-base-100">
-    <div class="card bg-secondary w-96 shadow-sm">
+  <div class="min-h-screen flex flex-col md:flex-row items-center justify-center gap-10 p-6 bg-base-100">
+    <a href="#/about/cadastromse" class="card bg-primary w-96 shadow-sm p-5 cursor-pointer hover:shadow-lg transition">
       <figure>
-        <img class="w-1/3 h-1/3"
-          src="./assets/mseicone.png">
+        <img class="w-1/3 h-1/3" src="./assets/mseicone.png" alt="icone medida">
       </figure>
-      <div class="card-body">
-        <h2 class="card-title">Cadastro de Medida</h2>
+      <div class="card-body flex items-center justify-center">
+        <h2 class="card-title text-center text-primary-content">Cadastro MSE</h2>
       </div>
-    </div>
+    </a>
 
-    <div class="card bg-accent w-96 shadow-sm">
+    <a href="#/about/cadastro-adolescente" class="card bg-secondary w-96 shadow-sm p-5 cursor-pointer hover:shadow-lg transition">
       <figure>
-        <img class="w-1/3 h-1/3"
-          src="./assets/adolescenteicone.png">
+        <img class="w-1/3 h-1/3" src="./assets/adolescenteicone.png" alt="icone adolescente">
       </figure>
-      <div class="card-body">
-        <h2 class="card-title">Cadastro de Adolescente</h2>
+      <div class="card-body flex items-center justify-center">
+        <h2 class="card-title text-center text-secondary-content-100">Cadastro de Adolescente</h2>
       </div>
-    </div>
-</div>
+    </a>
+  </div>
 </template>
+
 <script>
 
   export default {
